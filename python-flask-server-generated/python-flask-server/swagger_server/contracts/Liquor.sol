48d8538bbf79d7fa4e72f13d0633f71cca35b190
fugithora812 <playdrums.jump@gmail.com>
2020-12-10 16:35:53 +0900

    install openzeppelin

--- python-flask-server-generated/python-flask-server/swagger_server/contracts/Liquor.sol
+++ python-flask-server-generated/python-flask-server/swagger_server/contracts/Liquor.sol
@@ -1,4 +1,24 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.8.0;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "../node_modules/openzeppelin-solidity/contracts/token/ERC721/ERC721.sol";
+
+contract Liquor is ERC721 {
+    struct Liquor {
+        uint256 tokenId;
+        string liquorName;
+        // struct LiquorInfo {
+        //   string sellerName;
+        //   bool isReservable;
+        //   string arrivalDay;
+        //   int reserveScore;
+        // }
+    }
+
+    Liquor[] private liquorCollection;
+    address private _contractCreator;
+
+    constructor() public ERC721("Liquor", "LIQ") {
+        _contractCreator = msg.sender;
+    }
+}
