01cc00fcf97b3e0d444fa99e064a3df4bde4ac94
fugithora812 <playdrums.jump@gmail.com>
2020-12-10 17:35:59 +0900

    rewirte fetchDataFromBC

--- python-flask-server-generated/python-flask-server/swagger_server/dao/liquorDao.py
+++ python-flask-server-generated/python-flask-server/swagger_server/dao/liquorDao.py
@@ -4,3 +4,10 @@ import sqlite3
 
 class liquorDao:
     tokenId: string
+
+    def fetchDataFromBC(tokenId: int) -> string:
+        web3: Web3 = new Web3(web3.currentProvider)
+        # abi :string = コントラクトABI
+        # contractAddress :int = コントラクトアドレス
+        liquors = new web3py.eth.Contract(abi, contractAddress)
+        return liquors.methods.fetchLiquorData(tokenId).call()
