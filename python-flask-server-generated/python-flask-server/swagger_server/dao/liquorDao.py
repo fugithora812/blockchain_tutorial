5b54c1c505ed3ca934092faf5a39b37b11843ee7
fugithora812 <playdrums.jump@gmail.com>
2020-12-11 11:24:21 +0900

    update liquorDao.py (change fetchDataFromBC)

--- python-flask-server-generated/python-flask-server/swagger_server/dao/liquorDao.py
+++ python-flask-server-generated/python-flask-server/swagger_server/dao/liquorDao.py
@@ -1,5 +1,6 @@
 from web3 import Web3
 import sqlite3
+import json
 
 
 class liquorDao:
@@ -7,7 +8,14 @@ class liquorDao:
 
     def fetchDataFromBC(tokenId: int) -> string:
         web3: Web3 = new Web3(web3.currentProvider)
-        # abi :string = コントラクトABI
-        # contractAddress :int = コントラクトアドレス
-        liquors = new web3py.eth.Contract(abi, contractAddress)
+        pathToAbi = "../../BC_liquor/build/contracts"
+        # hash = "0xf35b44d4b8b87b6730fd3e12ddcf41cd69c64e97c0cdeebb1b8d93199658f5fb"
+        # print(web3.getTransactionReceipt(hash))
+
+        json_open = open(path, "r")
+        json_load = json.load(json_open)
+
+        abi = json_load["abi"]
+        contractAddress = "0xD12Ca9AaCBeFb6baD4de5A0D84FD324AD034bf40"
+        liquors = web3.eth.contract(address=contractAddress, abi=abi)
         return liquors.methods.fetchLiquorData(tokenId).call()
